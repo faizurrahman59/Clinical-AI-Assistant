adf20e6ec929177edce4cc8090d517efd7ebb075
faizurrahman59 <faizrahman4059@gmail.com>
2025-09-07 08:11:39 +0530

    update Readme file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="faizurrahman59",
+    author_email="faizrahman4059@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
